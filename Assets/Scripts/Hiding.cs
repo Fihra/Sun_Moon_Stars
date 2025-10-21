0a3577c7e2d6235472a1eafc57b6289016f15f4a
Fabian <fabrofabian@gmail.com>
2025-10-21 11:26:55 -0700

    added hiding mechanic but still a bit buggy, too fast subtracting the loot

---
+++ Assets/Scripts/Hiding.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class Hiding : MonoBehaviour
+{
+    [SerializeField] float safeCounter = 5;
+    float timer = 0;
+
+    private bool safety = false;
+    LightingManager currentDay;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("HidingSpot") || other.gameObject.CompareTag("LootRange"))
+        {
+            Debug.Log("Safe");
+            timer = 0;
+            safety = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("HidingSpot") || other.gameObject.CompareTag("LootRange"))
+        {
+            Debug.Log("Not Safe");
+            safety = false;
+        }
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentDay = LightingManager.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentDay.GetCurrentDay() == CurrentDay.Day)
+        {
+            safety = false;
+        } else if(currentDay.GetCurrentDay() == CurrentDay.Night)
+        {
+            safety = true;
+            timer = 0;
+        }
+
+        //Debug.Log("Timer: " + timer);
+
+        if (!safety)
+        {
+            
+            if (timer >= safeCounter)
+            {
+                ScoreManager.Instance.RemoveFromHeldLoot(.00001f);
+                return;
+                //Debug.Log("Im losing loot!");
+            }
+
+            timer += Time.deltaTime;
+        }
+
+  
+    }
+}
